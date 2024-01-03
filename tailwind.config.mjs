73830d02ea3cc1b151140c27612190a33d4b3278
Daniel Yepes <yepes1201@gmail.com>
2024-01-02 19:33:58 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        dark: "#141414",
+        card: "#272727",
+      },
+      textColor: {
+        primary: "#C1C1C1",
+      },
+    },
+  },
+  plugins: [],
+};
