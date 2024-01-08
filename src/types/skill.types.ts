9c449d197054731c74058a0c397978151b648fc6
Daniel Yepes <yepes1201@gmail.com>
2024-01-08 17:49:50 -0500

    refactor: separate types into a new file

---
+++ src/types/skill.types.ts
@@ -0,0 +1,14 @@
+export type SkillType =
+  | "frontend"
+  | "backend"
+  | "soft"
+  | "hard"
+  | "fullstack"
+  | "programmingl"
+  | "database"
+  | "tools";
+
+export interface TypeConfig {
+  title: string;
+  class: string;
+}
