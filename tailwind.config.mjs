ef5e9dbea7dfacd06c21a633052b9b6b5ef4e927
Daniel Yepes <yepes1201@gmail.com>
2024-01-11 21:14:59 -0500

    style: projects section mockup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,10 @@ export default {
         dark: "#141414",
         card: "#272727",
       },
+      backgroundImage: {
+        "project-one":
+          "url('https://images.unsplash.com/photo-1586880244406-556ebe35f282?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
     },
   },
   plugins: [],
